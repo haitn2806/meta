6653b86dd84f9ddfedbb69bc5598f49c32cdfad7
haitn2806 <haitn2806@gmail.com>
2022-12-12 13:41:14 +0700

    first commwwwi11t

--- pages/flags.js
+++ pages/flags.js
@@ -1,29 +1,29 @@
-// import React, { useState } from "react";
-// import ReactFlagsSelect from "react-flags-select";
-// import { Us } from "react-flags-select";
+import React, { useState } from "react";
+import ReactFlagsSelect from "react-flags-select";
+import { Us } from "react-flags-select";
 
-// const flags = () => {
-//   const [selected, setSelected] = useState("");
+const flags = () => {
+  const [selected, setSelected] = useState("");
 
-//   return (
-//     <div>
-//       <Us /> United States
-//       <div>asd</div>
-//       <ReactFlagsSelect
-//         countries={["US", "GB", "FR", "DE", "IT"]}
-//         customLabels={{
-//           US: "EN-US",
-//           GB: "EN-GB",
-//           FR: "FR",
-//           DE: "DE",
-//           IT: "IT",
-//         }}
-//         selected={selected}
-//         onSelect={(code) => setSelected(code)}
-//         placeholder="Select Language"
-//       />
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <Us /> United States
+      <div>asd</div>
+      <ReactFlagsSelect
+        countries={["US", "GB", "FR", "DE", "IT"]}
+        customLabels={{
+          US: "EN-US",
+          GB: "EN-GB",
+          FR: "FR",
+          DE: "DE",
+          IT: "IT",
+        }}
+        selected={selected}
+        onSelect={(code) => setSelected(code)}
+        placeholder="Select Language"
+      />
+    </div>
+  );
+};
 
-// export default flags;
+export default flags;
